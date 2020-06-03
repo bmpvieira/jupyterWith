91ffcfab466a5bf258bb3b4f60d25365dfffdab9
Bruno Vieira <mail@bmpvieira.com>
2020-06-03 13:44:51 +0100

    Update nixpkgs.nix url

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   src = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/f0bc52214879095e03f5256d2eb35796b549c048.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/467ce5a9f45aaf96110b41eb863a56866e1c2c3c.tar.gz";
     sha256 = "02p38pqv44i9inhskq5lmh6my3zilm21njdipv1dzms7k3mh47mn";
   };
 in
